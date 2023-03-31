db22e7c0c57f8c654506ab3d8e849f6dc1de60fe
Mustafa Akbari <100077297+MustafaAkbari@users.noreply.github.com>
2023-03-31 17:34:06 +0200

    Update __init__.py
    blue printe adde

--- __init__.py
+++ __init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 from main import main as main_blueprint
+from errors import error_bp as error_blueprint
 
 app.register_blueprint(main_blueprint)
+app.register_blueprint(error_blueprint)
 if __name__ == "__main__":
     app.run(debug=True)

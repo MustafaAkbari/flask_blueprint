b917b5a2f66512e95261072a3aea5c2b03f6991b
Mustafa Akbari <100077297+MustafaAkbari@users.noreply.github.com>
2023-03-31 17:37:15 +0200

    Update main.py
    dynamic routing created

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/users/<name>")
+def username(name):
+    return render_template("userpage.html", name=name)
